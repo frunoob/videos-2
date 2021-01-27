cd4c61ababa0ef0dbb2e91dfbf70693a45028069
frunoob <3024446880@qq.com>
2021-01-27 13:34:51 +0800

    20210127133448

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# videos-2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin git@github.com:frunoob/videos-2.git
+git push -u origin master
